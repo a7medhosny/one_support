0cc2c113a66502de0ab3c7bd7d2aa4708cbe9af2
a7medhosny <a7meddhosnyy@gmail.com>
2026-09-25 13:00:13 +0200

    chore(firebase): configure and initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:one_support/core/di/get_it.dart';
 import 'package:one_support/one_support_app.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await init();
 
   runApp(const OneSupportApp());
